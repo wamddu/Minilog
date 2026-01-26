78818f79157934aea8a834b212f31352bade0bd5
wamddu <yhy6923@naver.com>
2026-01-26 17:12:34 +0900

    Entity 이름 수정

--- src/main/java/com/asdf/minilog/entity/Follow.java
+++ src/main/java/com/asdf/minilog/entity/Follow.java
@@ -25,7 +25,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
-public class Follower {
+public class Follow {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
