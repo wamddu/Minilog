9fff23a2dbcc133dc44c703cf926875e9227209e
wamddu <yhy6923@naver.com>
2026-01-26 17:45:17 +0900

    Service 구현 중

--- src/main/java/com/asdf/minilog/service/UserService.java
+++ src/main/java/com/asdf/minilog/service/UserService.java
@@ -1,4 +1,54 @@
 package com.asdf.minilog.service;
 
+import com.asdf.minilog.dto.UserRequestDto;
+import com.asdf.minilog.dto.UserResponseDto;
+import com.asdf.minilog.entity.User;
+import com.asdf.minilog.repository.UserRepository;
+import com.asdf.minilog.util.EntityDtoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public List<UserResponseDto> getUsers() {
+        return userRepository.findAll().stream()
+                .map(EntityDtoMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<UserResponseDto> getUserById(Long userId) {
+        return userRepository.findById(userId).map(EntityDtoMapper::toDto);
+    }
+
+    public UserResponseDto createUser(UserRequestDto userRequestDto) {
+        if(userRepository.findByUsername(userRequestDto.getUsername()).isPresent()) {
+            throw new IllegalArgumentException("이미 존재하는 사용자입니다.");
+        }
+
+        User savedUser =
+                userRepository.save(
+                        User.builder()
+                                .username(userRequestDto.getUsername())
+                                .password(userRequestDto.getPassword())
+                                .build()
+                );
+
+        return EntityDtoMapper.toDto(savedUser);
+    }
 }
