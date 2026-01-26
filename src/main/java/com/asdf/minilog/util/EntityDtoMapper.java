c5ecefcb910ee4ff64185c4e79fd19ea1ea05fc6
wamddu <yhy6923@naver.com>
2026-01-26 17:12:46 +0900

    Mapper 구현

--- src/main/java/com/asdf/minilog/util/EntityDtoMapper.java
+++ src/main/java/com/asdf/minilog/util/EntityDtoMapper.java
@@ -1,4 +1,43 @@
 package com.asdf.minilog.util;
 
+import com.asdf.minilog.dto.ArticleResponseDto;
+import com.asdf.minilog.dto.FollowerRequestDto;
+import com.asdf.minilog.dto.FollowerResponseDto;
+import com.asdf.minilog.dto.UserResponseDto;
+import com.asdf.minilog.entity.Article;
+import com.asdf.minilog.entity.Follow;
+import com.asdf.minilog.entity.User;
+
 public class EntityDtoMapper {
+
+    public static ArticleResponseDto toDto(Article article){
+        return ArticleResponseDto.builder()
+                .articleId(article.getId())
+                .content(article.getContent())
+                .authorId(article.getAuthor().getId())
+                .authorName(article.getAuthor().getUsername())
+                .createdAt(article.getCreatedAt())
+                .build();
+    }
+
+    public static FollowerResponseDto toDto(Follow follow){
+        return FollowerResponseDto.builder()
+                .followerId(follow.getFollower().getId())
+                .followeeId(follow.getFollowee().getId())
+                .build();
+    }
+
+    public static UserResponseDto toDto(User user){
+        return UserResponseDto.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .build();
+    }
+
+    public static Follow toEntity(Long followerId, Long followeeId){
+        return Follow.builder()
+                .follower(User.builder().id(followerId).build())
+                .followee(User.builder().id(followeeId).build())
+                .build();
+    }
 }
