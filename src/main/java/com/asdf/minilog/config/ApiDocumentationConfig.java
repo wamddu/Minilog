5128ceaa25f59b4febc57e01ac59d2d1504880af
wamddu <yhy6923@naver.com>
2026-01-29 16:10:46 +0900

    Swagger 설정

--- src/main/java/com/asdf/minilog/config/ApiDocumentationConfig.java
+++ src/main/java/com/asdf/minilog/config/ApiDocumentationConfig.java
@@ -1,4 +1,21 @@
 package com.asdf.minilog.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ApiDocumentationConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("Minilog API")
+                                .version("1.0")
+                                .description("API documentation for the Minilog project")
+                );
+    }
 }
