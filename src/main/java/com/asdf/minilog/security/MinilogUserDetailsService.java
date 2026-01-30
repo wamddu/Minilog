f810f352cb219acd51608f95f24d36345b12e958
wamddu <yhy6923@naver.com>
2026-01-30 16:40:29 +0900

    JWT & 보안 구현

---
+++ src/main/java/com/asdf/minilog/security/MinilogUserDetailsService.java
@@ -0,0 +1,36 @@
+package com.asdf.minilog.security;
+
+import com.asdf.minilog.entity.User;
+import com.asdf.minilog.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class MinilogUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public MinilogUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        List<GrantedAuthority> authorities = user.getRoles().stream().map(MinilogGrantedAuthority::new).
+                collect(Collectors.toList());
+
+        return new MinilogUserDetails(user.getId(), user.getUsername(), user.getPassword(), authorities);
+    }
+}
